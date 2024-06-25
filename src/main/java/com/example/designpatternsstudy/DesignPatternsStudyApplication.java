e5c689419180715bdd2fd7517d72dd34052003d2
thisishwan2 <feel000617@gmail.com>
2024-06-25 14:12:13 +0900

    feat: Initial commit

---
+++ src/main/java/com/example/designpatternsstudy/DesignPatternsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.designpatternsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsStudyApplication.class, args);
+    }
+
+}
